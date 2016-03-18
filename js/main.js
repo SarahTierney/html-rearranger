1aa1f8c9c9e850f4f047c52b03b4e88d86867291
SarahTierney <tierney.s@live.com>
2016-03-17 23:21:03 -0400

    added more js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,20 @@
-var $h1a = $('<h1>');
+var $h1A = $('<h1>');
 var $body = $('<body>');
-var $ula = $('<ul>');
-var $h1b = $('<h1>');
-var $ulb = $('<ul>');
+var $ulA = $('<ul>');
+var $h1B = $('<h1>');
+var $ulB = $('<ul>');
 
-$h1a.html('Below ground veggies');
-$body.append()
+$h1A.html('Below ground veggies');
+$body.append($h1A);
+$body.append($ulA);
+$h1B.html('Above ground veggies');
+$body.append($h1B);
+$body.append($ulB);
+
+$('ul li').each(function () {
+  if ($(this).hasClass('below')){
+    $ulA.append($(this));
+  } else {
+    $ulB.append( )
+  }
+})
